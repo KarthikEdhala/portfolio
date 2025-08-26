89ab6091fcaea633b10de7151e0ce01a5d85f90d
Edhala <karthik@aios.agency>
2025-08-26 17:13:36 -0400

    Added icon infront of the url

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,29 @@
-import { Outfit, Ovo} from "next/font/google";
+// import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
-const outfit = Outfit({
-  subsets: ["latin"], weight: ["400", "500", "600", "700"]
-});
+// const outfit = Outfit({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700"],
+// });
 
-const ovo = Ovo({
-  subsets: ["latin"], weight: ["400"]
-});
-
-
+// const ovo = Ovo({
+//   subsets: ["latin"],
+//   weight: ["400"],
+// });
 
 export const metadata = {
-  title: "Portfolio - GreatStack",
+  title: "Karthik Edhala",
   description: "",
+  icons: {
+    icon: "/favicon.webp",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+        className={`antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
       </body>
